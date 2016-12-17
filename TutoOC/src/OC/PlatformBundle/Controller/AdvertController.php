d771bd8adf9a6f9b0093d7bcc3f157913ed6bdc5
Hifumisama <m.khayat92@gmail.com>
2016-12-17 19:31:47 +0100

    Ajout du bundle OCPlatformBundle (généré via la console), suppression du Appbundle et toutes ses traces (inutile car faisant partie d'une démo de symfony). Introduction des routes, du contrôleur et des vues dans Synfony avec la création du AdvertController.php ainsi que de index.html.twig pour la vue.

---
+++ TutoOC/src/OC/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,30 @@
+<?php 
+
+namespace OC\PlatformBundle\Controller;
+
+// on annonce que l'on va utiliser l'objet response (qui permet de retourner d'afficher quelque chose dans notre cas.)
+use Symfony\Component\HttpFoundation\Response;
+
+// Cet objet permet cette fois de générer nos vues en .twig.
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+// On crée notre classe AdvertController dans laquelle va se trouver les méthodes que l'on va utiliser, La classe contient son nom ainsi que le suffixe Controller, ajouté pour que Symfony la reconnaisse comme tel.
+
+// Pourquoi l'extends ? : Car on a hérité des caractéristiques et des méthodes du controleur de base tout simplement.
+
+class AdvertController extends Controller
+{
+    // on crée notre méthode dans la classe advert, qui contient également son nom suivi du suffixe Action. qui définit son rôle. 
+    
+    public function HelloWorldAction()
+    {
+        // On se contente de créer un message indiquant que le contrôleur fonctionne correctement.
+        return new Response("Notre Propre Hello World fonctionne :o !");
+    }
+    
+    public function indexAction()
+    {
+        $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig');
+        return new Response($content);
+    }
+}
